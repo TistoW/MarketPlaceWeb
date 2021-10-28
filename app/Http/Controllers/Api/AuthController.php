2fccfdefa4d8c8eb9b57bfe1307bb043bffadcb2
Inyong Tisto <imtisto@gmail.com>
2021-10-28 10:37:38 +0700

    - create Register API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,18 +5,65 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
-class AuthController extends Controller
-{
-    public function login(Request $request){
-        if(!$request->email){
-            return "Email tidak boleh kosong";
+class AuthController extends Controller {
+    public function login(Request $request) {
+        $validasi = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
         }
 
         $user = User::where('email', $request->email)->first();
-        if($user){
-            return "Selamat datang ".$user->name;
+        if ($user) {
+            if (password_verify($request->password, $user->password)) {
+                return $this->success($user);
+            } else {
+                return $this->error("Password salah");
+            }
         }
-        return "User tidak di temukan";
+        return $this->error("User tidak di temukan");
+    }
+
+    public function register(Request $request) {
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'phone' => 'required|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
+        }
+
+        $user = User::create(array_merge($request->all(), [
+            'password' => bcrypt($request->password)
+        ]));
+
+        if ($user) {
+            return $this->success($user, 'selamat datang ' . $user->name);
+        } else {
+            return $this->error("Terjadi kesalahan");
+        }
+    }
+
+    public function success($data, $message = "success") {
+        return response()->json([
+            'code' => 200,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    public function error($message) {
+        return response()->json([
+            'code' => 400,
+            'message' => $message
+        ], 400);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
